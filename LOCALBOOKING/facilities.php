ad23f110011216a846f662b83ae1e32c1a773f8f
illemon <roylandmandate12@gmail.com>
2024-10-16 10:02:05 +0800

    Add files via upload

---
+++ LOCALBOOKING/facilities.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset = "UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> LOCAL BOOKINGS - FACILITIES </title>
+    <?php require('inc/links.php'); ?>
+    <style>
+        .pop:hover{
+            border-top-color: var(--teal) !important;
+            transform: scale(1.03);
+            transition: all 0.3s;
+        }
+    </style>
+
+</head>
+<body class="bg- light">
+
+    <?php require('inc/header.php'); ?>
+
+    <div class="my-5 px-4">
+        <h2 class="fw-bold text-center">OUR FACILITIES</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Incidunt alias ullam <br> eaque maxime quidem veniam corrupti 
+            repellat, neque nostrum odio!
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2"> 
+                        <img src="images/features/wifi.svg" width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Magnam repellat dolorem vero quas nam nobis vel.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2"> 
+                        <img src="images/features/ac.svg" width="40px">
+                        <h5 class="m-0 ms-3">Air Conditioner</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Magnam repellat dolorem vero quas nam nobis vel.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2"> 
+                        <img src="images/features/heater.svg" width="40px">
+                        <h5 class="m-0 ms-3">Heater</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Magnam repellat dolorem vero quas nam nobis vel.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2"> 
+                        <img src="images/features/IMG_47816.svg" width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Magnam repellat dolorem vero quas nam nobis vel.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2"> 
+                        <img src="images/features/IMG_96423.svg" width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Magnam repellat dolorem vero quas nam nobis vel.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2"> 
+                        <img src="images/features/tv.svg" width="40px">
+                        <h5 class="m-0 ms-3">TV</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Magnam repellat dolorem vero quas nam nobis vel.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <?php require('inc/footer.php'); ?>
+
+</body>
+</html>
---
+++ LOCALBOOKING/inc/footer.php
@@ -0,0 +1,35 @@
+<div class="container-fluid bg-white mt-5">
+    <div class="row">
+        <div class="col-lg-4 p-4">
+            <h3 class="fw-bold fs-3 h-font">WiseBooking</h3>
+            <p>
+                lorem ipsum  dolor sit amet, consectetur adipisicing elit.
+                lorem ipsum  dolor sit amet, consectetur adipisicing elit.
+                lorem ipsum  dolor sit amet, consectetur adipisicing elit.
+            </p>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Links</h5>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Book</a> <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a> <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About us</a>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Follow us</h5>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-twitter-x me-1"></i>Twitter-X
+            </a><br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-facebook me-1"></i>Facebook
+            </a><br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none">
+                <i class="bi bi-instagram me-1"></i>Instagram
+            </a><br>
+        </div>
+    </div>
+</div>
+
+<h6 class="text-center bg-dark text-white p-3 m-0">Designed and Develop by THE IDIOTS</h6>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
